de30f98fb39e90b6eb7d4555c949aa774870861d
tanx16 <tanx@berkeley.edu>
2018-02-17 05:45:45 -0800

    Minor changes

--- dotfilesetup.sh
+++ dotfilesetup.sh
@@ -2,8 +2,8 @@
 mkdir -p ~/.vim/autoload ~/.vim/bundle && \
 curl -LSso ~/.vim/autoload/pathogen.vim https://tpo.pe/pathogen.vim
 
-mv ./.vimrc ~/.vimrc
-mv ./tmux.conf ~/.tmux.conf
+cp ./.vimrc ~/.vimrc
+cp ./.tmux.conf ~/.tmux.conf
 
 git clone https://github.com/scrooloose/nerdtree.git ~/.vim/bundle/nerdtree
 git clone https://github.com/morhetz/gruvbox.git ~/.vim/bundle/gruvbox
