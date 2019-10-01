e7dd64b02144ace28001a88c856d19b5c300eb68
Xufei Tan <tanx@yelp.com>
2019-10-01 16:01:56 -0700

    adding bashrc

--- install.sh
+++ install.sh
@@ -4,9 +4,11 @@ curl -LSso ~/.vim/autoload/pathogen.vim https://tpo.pe/pathogen.vim
 
 cp ~/.vimrc ~/.vimrc.bak
 cp ~/.tmux.conf ~/.tmux.conf.bak
+cp ~/.bashrc ~/.bashrc.bak
 
 cp ./.vimrc ~/.vimrc
 cp ./.tmux.conf ~/.tmux.conf
+cp ./.bashrc ~/.bashrc
 
 git clone https://github.com/scrooloose/nerdtree.git ~/.vim/bundle/nerdtree
 git clone https://github.com/morhetz/gruvbox.git ~/.vim/bundle/gruvbox
